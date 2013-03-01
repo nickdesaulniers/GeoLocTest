17797f9a1d254f7e5a281dc2332401c48ff9865b
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-03-01 15:36:35 -0800

    add the ability to clear the screen

--- geo.js
+++ geo.js
@@ -1,24 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var domWrite = function domWrite (msg) {
-    document.body.appendChild(document.createTextNode(msg));
-  };
-  if (navigator.geolocation) {
-    domWrite('navigator.geolocation exists;');
-    navigator.geolocation.getCurrentPosition(function success (position) {
-      let [lat, lon] = [position.coords.latitude, position.coords.longitude];
-      domWrite('lat: ' + lat + ', lon:' + lon);
-    },
-    function error (error) {
-      switch (error.code) {
-        case 0: domWrite('Unknown Error'); break;
-        case 1: domWrite('Permission Denied'); break;
-        case 2: domWrite('Position Unavailable'); break;
-        case 3: domWrite('Timed Out'); break;
-        default: domWrite('Unknown error code: ' + error.code); break;
-      }
-    },
-    { maximumAge: 600000 });
-  } else {
-    domWrite('No navigatior.geolocation');
-  }
+  let $ = document.getElementById.bind(document),
+      logEle = $('log'),
+      domWrite = function domWrite (msg) {
+        log.appendChild(document.createTextNode(msg));
+        log.appendChild(document.createElement('br'));
+      },
+      getCurrentPositionSuccess = function getCurrentPositionSuccess (position) {
+        let [lat, lon] = [position.coords.latitude, position.coords.longitude];
+        domWrite('lat: ' + lat + ', lon:' + lon);
+      },
+      error = function error (e) {
+        switch (e.code) {
+          case 0: domWrite('Unknown Error'); break;
+          case 1: domWrite('Permission Denied'); break;
+          case 2: domWrite('Position Unavailable'); break;
+          case 3: domWrite('Timed Out'); break;
+          default: domWrite('Unknown error code: ' + e.code); break;
+        }
+      },
+      getCurrentPosition = function getCurrentPosition () {
+        navigator.geolocation.getCurrentPosition(getCurrentPositionSuccess,
+          error, { maximumAge: 600000 });
+      },
+      clear = function clear () {
+        logEle.innerHTML = '';
+      };
+
+  $('getCurrentPosition').addEventListener('click', getCurrentPosition);
+  $('clear').addEventListener('click', clear);
+  domWrite('navigator.geolocation ' + (navigator.geolocation ? 'exists' : 'DNE'));
 });
