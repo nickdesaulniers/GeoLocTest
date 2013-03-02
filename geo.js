2a7152630a47b16e8cf2a4d33cb6ce1c4d82ff6a
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-03-01 16:07:48 -0800

    added watchPosition

--- geo.js
+++ geo.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
+  if (!navigator.geolocation) return;
   let $ = document.getElementById.bind(document),
       logEle = $('log'),
+      watchPositionEle = $('watchPosition'),
+      watchID = null,
       domWrite = function domWrite (msg) {
         log.appendChild(document.createTextNode(msg));
         log.appendChild(document.createElement('br'));
       },
-      getCurrentPositionSuccess = function getCurrentPositionSuccess (position) {
+      clear = function clear () {
+        logEle.innerHTML = '';
+      },
+      success = function success (position) {
         let [lat, lon] = [position.coords.latitude, position.coords.longitude];
         domWrite('lat: ' + lat + ', lon:' + lon);
       },
@@ -18,15 +24,21 @@ document.addEventListener('DOMContentLoaded', function () {
           default: domWrite('Unknown error code: ' + e.code); break;
         }
       },
+      config = { timeout: 5000, enableHighAccuracy: false, maximumAge: 1000 },
       getCurrentPosition = function getCurrentPosition () {
-        navigator.geolocation.getCurrentPosition(getCurrentPositionSuccess,
-          error, { maximumAge: 600000 });
+        navigator.geolocation.getCurrentPosition(success, error, config);
       },
-      clear = function clear () {
-        logEle.innerHTML = '';
+      watchPosition = function watchPosition () {
+        if (watchPositionEle.textContent === 'stop') {
+          watchPositionEle.textContent = 'watchPosition';
+          navigator.geolocation.clearWatch(watchID);
+        } else {
+          watchPositionEle.textContent = 'stop';
+          watchID = navigator.geolocation.watchPosition(success, error, config);
+        }
       };
 
   $('getCurrentPosition').addEventListener('click', getCurrentPosition);
+  watchPositionEle.addEventListener('click', watchPosition);
   $('clear').addEventListener('click', clear);
-  domWrite('navigator.geolocation ' + (navigator.geolocation ? 'exists' : 'DNE'));
 });
